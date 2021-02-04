59889ad6877470a9425b67096cd48d0ef585519e
Elliott Vargo <vargoelliott@gmail.com>
2021-02-04 01:12:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,45 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+      self.all << song
+      song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+
+  end
+
+  def self.create_by_name(title)
+    song = self.create
+   song.name = title
+   song
+  end
+
+  def self.find_by_name(title)
+    self.all.detect {|t| t.name == title}
+  end
+
+  def self.find_or_create_by_name(title)
+      if find_by_name(title)
+        return find_by_name(title)
+      else
+         create_by_name(title)
+      end
+    end
+
+  def self.alphabetical
+    all.sort_by {|s| s.name}
+  end
+
+  def self.new_from_filename(string)
+     new_string = string.delete(/.mp3/)
+     array = new_string.split(" - ")
+    artist_name = array[0]
+    new_by_name(array[1])
+  end
 end
